9829a02199696f580eacd12c4ae681423a8a744e
Luis Fernando <luis.betancourt@ank.app>
2022-06-27 11:23:11 -0300

    Initial Commit

---
+++ Shared/rick_and_morty_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  rick_and_morty_appApp.swift
+//  Shared
+//
+//  Created by luis.betancourt on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct rick_and_morty_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
